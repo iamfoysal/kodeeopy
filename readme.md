b2423a7b8375989c39c230d77ba7a515e08ea3a2
foysal <foysalf652@gmail.com>
2022-02-28 01:43:49 +0600

    up to date

--- readme.md
+++ readme.md
@@ -1,21 +1,8 @@
-1. pipenv install Command - "pip install pipenv"
-2. virtual environment install and activate - "pipenv shell" 
-3. then install Django - "pipenv install django  your_version"
-4. install mysqlclient - "pipenv install mysqlclient "
+
 
 #Create virtual environment with pipenv
 ###we should follow this doc
 #### write some commands
-<<<<<<< HEAD
+
  ##pipenv install Command  
      -  pip install pipenv
-
-
-
-
-    
-=======
-   pipenv install Command  
-     - pip install pipenv
-    
->>>>>>> 080d35cc66475c9a485e9b3934da1ad9efe2b608
--- test_core/settings.py
+++ test_core/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +75,8 @@ WSGI_APPLICATION = 'test_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todo-app',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
